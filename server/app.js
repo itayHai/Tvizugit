83179d120d7ce84f2c748b1b00b8b8bbb5ffdca3
Itay Haizler <itay123987@gmail.com>
2020-04-06 15:31:47 +0300

    create template client side

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 const app = express()
-const port = 3000;
+const port = 8000;
 
 app.get('/', (req, res) => res.send('Hello World'))
 app.listen(port, () => console.log(`Server is running on port ${port}`))
