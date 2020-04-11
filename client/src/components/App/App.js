492d31dfd7bbe83bb433fc49da4a1b7df643a913
Itay Haizler <itay123987@gmail.com>
2020-04-11 16:23:51 +0300

    Create navbar with router

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,14 +1,74 @@
 import React from 'react';
-import logo from '../../images/logo.svg';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import './App.css';
+import Navbar from '../navbar/navbar'
+import Background from '../../images/home-background.jpg';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+var sectionStyle = {
+  width: "100%",
+  height: "500px",
+  backgroundImage: `url(${ Background })`,
+  backgroundSize: '100% 600px',
+  backgroundRepeat: 'no-repeat',
+  
+};
+
+const theme = createMuiTheme({
+   typography: {
+      fontFamily:
+        'Segoe UI',
+      },
+   palette: {
+    primary: {
+      light: '#6abba7',
+      main: '#45AB92',
+      dark: '#307766',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#7f97a2',
+      main: '#607d8b',
+      dark: '#435761',
+      contrastText: '#000',
+    },
+  },
+  direction: 'rtl', // Both here and <body dir="rtl">
+});
+
+const styles = { 
+  width: '85%',
+  margin: 'auto',
+  height: '100%'
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <div style={styles} className="App">
+          <Navbar/>
+          <Switch>
+              <Route exact path="/">
+                <section style={ sectionStyle }>
+                </section>
+              </Route>
+              <Route path="/cases">
+                <h1>Cases</h1>
+              </Route>
+              <Route path="/lawyers">
+                <h1>Lawyers</h1>
+              </Route>
+          </Switch>
+         
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ client/src/components/navbar/navbar.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar';
+import {Toolbar, Button } from '@material-ui/core'
+import GavelIcon from '@material-ui/icons/Gavel';
+import PersonIcon from '@material-ui/icons/Person';
+import { withRouter } from "react-router";
+import { Link } from "react-router-dom";
+import './navbar.css' 
+
+function Navbar() {
+  return (
+      <AppBar position="static">
+        <Toolbar className="navbar">
+          <Link to="/" className="link">
+            <GavelIcon className="icon" fontSize="large"/>
+            <div className="titleContainer">
+              <label className="mainTitle">תביצוגית</label>
+              <label className="slogan">לתבוע בידיים טובות</label>
+            </div>
+          </Link>
+          <Link to="/cases" className="link" >
+            <h3>מאגר התביעות</h3>
+          </Link>
+          <Link to="/lawyers" className="link" >
+            <h3>מאגר עורכי הדין</h3>
+          </Link>     
+          <Button className="login"
+            variant="contained"
+              color="secondary"
+            startIcon={<PersonIcon />}
+          >
+            כניסה
+          </Button>    
+        </Toolbar>
+      </AppBar>
+  );
+}
+
+export default withRouter(Navbar)
