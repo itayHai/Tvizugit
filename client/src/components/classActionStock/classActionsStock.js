43e16267c4c1c5257f3e4d7fbd6f3384bbb1704d
ellaportnov <ellaportnov@gmail.com>
2020-04-13 18:04:54 +0300

    wip

---
+++ client/src/components/classActionStock/classActionsStock.js
@@ -0,0 +1,10 @@
+import React from "react";
+import SearchHeader from '../UI/searchHeader/searchHeader';
+
+const classActionsStock = () => {
+  return <div>
+    <SearchHeader title='מאגר תביעות'/>
+  </div>;
+};
+
+export default classActionsStock;
