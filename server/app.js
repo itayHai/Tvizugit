1d748fa42e71a3453a47a7c788af87bf2b250a81
ellaportnov <ellaportnov@gmail.com>
2020-04-13 14:42:20 +0300

    added database connection

--- server/app.js
+++ server/app.js
@@ -1,7 +1,19 @@
-import express from 'express';
+const express = require('express');
+
+const path = require('path');
+
+const bodyParser = require('body-parser');
+
+const mongoConnect = require('./utils/database');
 
 const app = express()
 const port = 8000;
 
-app.get('/', (req, res) => res.send('Hello World'))
-app.listen(port, () => console.log(`Server is running on port ${port}`))
+app.get('/', (req, res) => res.send('Hello World!!'))
+
+
+mongoConnect((client) => {
+    console.log(client);
+    app.listen(port, () => console.log(`Server is running on port ${port}`))
+
+});
