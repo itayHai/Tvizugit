62166ddadd0d3f54fb19412cbb79dc4f6a4b22a2
ellaportnov <ellaportnov@gmail.com>
2020-04-13 14:42:20 +0300

    added database connection

--- server/app.js
+++ server/app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require('path');
+
+const bodyParser = require('body-parser');
+
+const mongoConnect = require('./utils/database');
 
 const app = express();
 const port = 8000;
@@ -8,5 +13,6 @@ app.get("/", (req, res) => res.send("Hello World!!"));
 
 mongoose.connect("mongodb+srv://Tvizugit:4RmpQFkU4XX9PLgH@cluster0-2kbif.mongodb.net/Tvizugit?retryWrites=true&w=majority")
   .then(
+    console.log(client);
     app.listen(port, () => console.log(`Server is running on port ${port}`))
   ).catch(err => console.log(err));
