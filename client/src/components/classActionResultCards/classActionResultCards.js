58322df7f4c0a837892b25a34c760ccad052f72d
idoperlman <idoperlman2@gmail.com>
2020-04-14 17:48:30 +0300

    wip

---
+++ client/src/components/classActionResultCards/classActionResultCards.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ClassActionResultCard from '../classActionResultCard/classActionResultCard';
+
+const classActionResultCards = props => {
+    // Just for QA, supposed to come from porps
+    let checkActions = [{
+        Id: "1",
+        actionName: "תביעת המאה",
+        defendantParty: "שטראוס",
+        actionStage: "תוך כדי משפט",
+        numberOfProsecutors: "10,000"
+    }, {
+        Id: "2",
+        actionName: "תביעת המאה",
+        defendantParty: "facebook",
+        actionStage: "לפני משפט",
+        numberOfProsecutors: "2000"
+    }];
+    const classActions = checkActions.map((cAction) => {
+        return <ClassActionResultCard
+            classAction={cAction}
+            key={cAction.Id}
+        />
+    });
+
+    return (
+        <div>
+        { classActions }
+        </div>  
+    );
+};
+
+
+export default classActionResultCards;
