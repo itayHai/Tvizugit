dff570d272125a6d4766259da28a6ff5e7631141
idoperlman <idoperlman2@gmail.com>
2020-04-15 23:58:30 +0300

    wip

--- client/src/components/classActionStock/classActionsStock.js
+++ client/src/components/classActionStock/classActionsStock.js
@@ -2,34 +2,17 @@ import React from "react";
 import SearchHeader from '../searchHeader/searchHeader';
 import ClassActionResultCards from './classActionResultCards/classActionResultCards';
 import classes from './classActionStock.module.css';
+import { propertiesToShow } from '../../utils/globalConsts';
 
 const classActionsStock = (props) => {
-
-  let basicProperties = [{
-    value: "שם התביעה",
-    key: "actionName"
-  },
-  {
-    value: "הגוף הנתבע",
-    key: "defendantParty"
-  },
-  {
-    value: "שלב התביעה",
-    key: "actionStage"
-  },
-  {
-    value: "מספר תובעים",
-    key: "numberOfProsecutors"
-  }];
-
-
+  
   return <div className={classes.page}>
     <SearchHeader
       title='מאגר תביעות'
-      itemsToSelect={basicProperties}
+      itemsToSelect={propertiesToShow.find(m => m.name === "classAction").classActionPro}
     />
     <ClassActionResultCards
-      basicProperties={basicProperties}
+      basicProperties={propertiesToShow.find(m => m.name === "classAction").classActionPro}
     />
   </div>;
 };
