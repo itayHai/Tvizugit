626fb822f2d3c61e91a54db9b21e7801b1b5a967
ellaportnov <ellaportnov@gmail.com>
2020-04-15 18:04:02 +0300

    added generic Modal

--- client/src/components/modal/modal.js
+++ client/src/components/modal/modal.js
@@ -1,18 +1,18 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Modal from "@material-ui/core/Modal";
-import Backdrop from "@material-ui/core/Backdrop";
-import Fade from "@material-ui/core/Fade";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 
 const useStyles = makeStyles((theme) => ({
   modal: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   paper: {
     backgroundColor: theme.palette.background.paper,
-    border: "2px solid #000",
+    border: '2px solid #000',
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
   },
@@ -20,10 +20,12 @@ const useStyles = makeStyles((theme) => ({
 
 export default function TransitionsModal(props) {
   const classes = useStyles();
+ 
 
   return (
     <div>
-      <Modal
+     
+      <Modal 
         aria-labelledby="transition-modal-title"
         aria-describedby="transition-modal-description"
         className={classes.modal}
@@ -37,7 +39,6 @@ export default function TransitionsModal(props) {
       >
         <Fade in={props.show}>
           <div className={classes.paper}>
-            <h1>{props.title}</h1>
             {props.children}
           </div>
         </Fade>
