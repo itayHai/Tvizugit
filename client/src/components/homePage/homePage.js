9be0f3dd005d927e8ca743fe24c46e927ecd4076
Itay Haizler <itay123987@gmail.com>
2020-04-17 14:47:26 +0300

    add the classActionCards to homepage

---
+++ client/src/components/homePage/homePage.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+import classes from './homePage.module.css'
+import { Button } from '@material-ui/core'
+import ClassActionsByFilter from '../classActionsByFilter/classActionsByFilter'
+
+const CONSTS = {
+    filters: {
+        "MOST_PROMINENT": "MOST_PROMINENT",
+        "LOGGED_USER": "LOGGED_USER",
+    }
+}
+
+export default function homePage() {
+    return (
+        <div>
+            <section className={classes.section}>
+                <div className={classes.text}>
+                    <Typography className={classes.headLine} variant="h4">נפגעת? חלית? אולי מגיעים לך פיצויים מתביעה ייצוגית!</Typography>
+
+                    <label className={classes.para}>
+                        כל כך הרבה תובענות ייצוגיות נתבעות ונפסקות,
+                    </label>
+                    <label className={classes.para}>
+                        בלי שבכלל נדע שמגיע לנו פיצויים בזכותן!
+                    </label>
+                    <Button 
+                        className={classes.sectionButtons}
+                        variant="contained"
+                        color="secondary"
+                        size="large"
+                    >
+                        בדוק את זכאותך עוד היום
+                    </Button>
+                    <Button 
+                        className={classes.sectionButtons}
+                        variant="contained"
+                        color="secondary"
+                        size="large"
+                    >
+                        גיבוש תובענה ייצוגית חדשה
+                    </Button>
+                </div>
+            </section>
+            <article className={classes.article}>
+                <p className={classes.classActionTitle}>
+                    התובענות הבולטות ביותר
+                </p>
+                <ClassActionsByFilter
+                    filter={ CONSTS.filters.MOST_PROMINENT }
+                    size={ 4 }
+                />
+            </article>
+        </div>
+    )
+}
