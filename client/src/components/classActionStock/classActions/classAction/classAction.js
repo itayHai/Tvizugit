9a7b8e7dc374138fb0673c64fcd8fc5c21e48be5
idoperlman <idoperlman2@gmail.com>
2020-04-18 09:51:11 +0300

    add joinAction module
    appears if the user is not in the action

--- client/src/components/classActionStock/classActions/classAction/classAction.js
+++ client/src/components/classActionStock/classActions/classAction/classAction.js
@@ -3,18 +3,19 @@ import ResultBanner from '../../../resultBanner/resultBanner';
 import { propertiesToShow } from '../../../../utils/globalConsts';
 import ClassActionContent from '../classAction/classActionContent/classActionContent';
 
-const classAction = props => {
+const ClassAction = props => {
+
     const basicProperties = propertiesToShow[0].classActionPro;
-    // TODO: this or send in children
+
     const selectedProperties = basicProperties.map((bProp) => {
         return { ...bProp, content: props.classAction[bProp.key] };
     });
-
     return (
-        <ResultBanner selectedProperties={selectedProperties}>
-            <ClassActionContent cAction={props.classAction} />
-        </ResultBanner>
+            <ResultBanner selectedProperties={selectedProperties} 
+            >
+                <ClassActionContent cAction={props.classAction} />
+            </ResultBanner>
     );
 };
 
-export default classAction;
+export default ClassAction;
