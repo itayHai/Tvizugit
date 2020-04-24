5fd1ac29941327feafe6e2672e0b7db6df6c4ac6
rotemhugi <rotemhugi@gmail.com>
2020-04-24 22:57:18 +0300

    RegisterLawyerScreenUI

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -3,6 +3,7 @@ import Button from "@material-ui/core/Button";
 import Link from "@material-ui/core/Link";
 import Input from "@material-ui/core/Input";
 import PersonIcon from '@material-ui/icons/Person';
+import GavelIcon from '@material-ui/icons/Gavel';
 import FacebookIcon from '../../images/icons/facebook_icon.png';
 import GoogleIcon from '../../images/icons/google_icon.png';
 import { users } from "../../utils/globalConsts";
@@ -10,32 +11,53 @@ import UserCard from "../userCard/userCard";
 import classes from "./login.module.css"
 
 function Login (props) {
-    let allusers = [];
-    // Get the lawyer box
-    allusers.push(<div key={0}> <UserCard key={users[0].id}
-                                          icon={users[0].icon}
-                                          title={users[0].name}
-                  /></div>);
-    
-    // Get the user box
-    allusers.push(<div key={1}> <UserCard key={users[1].id}
-                                          icon={users[1].icon}
-                                          title={users[1].name}
-                  /></div>);   
-    
-    const [showLogin,SetShowLogin] = useState(true)
 
-    function changeToSignUp(){
-        SetShowLogin(false);
-    }
+const screen = {
+  login: 1,
+  regiester: 2,
+  lawyer: 3,
+  user: 4
+}
+    
+  let allusers = [];
 
-    function changeToLogin(){
-        SetShowLogin(true);
-    }
+  // Get the lawyer box
+  allusers.push(<div key={0} 
+                     onClick={regiesterAsLawyer}> 
+                     <UserCard key={users[0].id}
+                               icon={users[0].icon}
+                               title={users[0].name}
+                 /></div>);
+    
+  // Get the user box
+  allusers.push(<div key={1}
+                     onClick={regiesterAsUser}> 
+                     <UserCard key={users[1].id}
+                               icon={users[1].icon}
+                               title={users[1].name}
+                 /></div>);   
+
+  const [mode,SetMode] = useState("login")
+
+  function changeToRegister(){
+    SetMode("register");
+  }
+
+  function changeToLogin(){
+    SetMode("login");
+  }
+
+  function regiesterAsLawyer(){
+    SetMode("lawyer");
+  }
+
+  function regiesterAsUser(){
+    SetMode("user");
+  }
 
   return (
     <div>
-    <div style={{display: showLogin? "block" : "none"}}>
+    <div style={{display: mode == "login" ? "block" : "none"}}>
       <div className={classes.Center}>
         <h2> <PersonIcon/> כניסה לאתר</h2>
         <hr color="#e6e6e6"/>
@@ -60,16 +82,16 @@ function Login (props) {
       </p>
       <br/><hr color="#e6e6e6"/>
       <div className={classes.Center}>
-        <a>או כניסה דרך הרשתות החברתיות</a>
+        <label>או כניסה דרך הרשתות החברתיות</label>
         <p>
-            <img className={classes.LogoIcon} src={FacebookIcon}/>
+            <img className={classes.LogoIcon} src={FacebookIcon} alt="FaceBook"/>
             <span> </span>
-            <img className={classes.LogoIcon} src={GoogleIcon}/>
+            <img className={classes.LogoIcon} src={GoogleIcon} alt="Google"/>
         </p>
-        <Link onClick={changeToSignUp}>אין לך חשבון?</Link>
+        <Link onClick={changeToRegister}>אין לך חשבון?</Link>
       </div>
     </div>
-    <div style={{display: showLogin? "none" : "block"}}>
+    <div style={{display: mode == "register" ? "block" : "none"}}>
     <div className={classes.Center}>
         <h3>אנחנו שמחים שבחרת להירשם לתביצוגית!</h3>
         <h4>איזה סוג חשבון מתאים לך?</h4>
@@ -83,6 +105,38 @@ function Login (props) {
       </p>
       <Link onClick={changeToLogin}>כבר יש לך חשבון?</Link>
       </div>
+    </div>
+    <div style={{display: mode == "lawyer" ? "block" : "none"}}>
+      <div className={classes.Center}>
+        <h2> <GavelIcon/> יצירת פרופיל עו"ד בתביצוגית</h2>
+        <hr color="#e6e6e6"/>
+      </div>
+      <Input placeholder="שם המשרד"
+             className={classes.Input}
+             autoFocus={true}
+      /><br/>
+      <Input placeholder="תיאור המשרד"
+             className={classes.Input}
+      /><br/>
+      <Input placeholder="תחומי ההתמחות של המשרד"
+             className={classes.Input}
+      /><br/>
+      <Input placeholder="כתובת המשרד"
+             className={classes.Input}
+      /><br/>
+      <Input placeholder="טלפון המשרד"
+             className={classes.Input}
+      /><br/>
+      <Input placeholder="ותק המשרד"
+             className={classes.Input}
+      />
+      <p>
+      <Button className={classes.LoginButton} 
+              variant="contained" 
+              onClick={props.close}>
+        יצירת פרופיל
+      </Button>
+      </p>
     </div>
     </div>
   );
