285fbc41233914647aa380bf016c7cd17437b55f
rotemhugi <rotemhugi@gmail.com>
2020-04-25 21:16:05 +0300

    fix ui

--- client/src/components/login/login.js
+++ client/src/components/login/login.js
@@ -12,21 +12,21 @@ import { TextField } from "@material-ui/core";
 
 function Login (props) {
 
-const [hashtags] = useState([]);
-const [mode,SetMode] = useState("login")
-const [value, setValue] = useState("");
+  const [hashtags] = useState([]);
+  const [mode,SetMode] = useState("login")
+  const [value, setValue] = useState("");
 
-const keyDownHandler = (event) => {
-  if (["Enter", "Tab", ","].includes(event.key)) {
-    event.preventDefault();
-    hashtags.push(value.trim());
-    setValue("");
-  }
-};
+  const keyDownHandler = (event) => {
+    if (["Enter", "Tab", ","].includes(event.key)) {
+      event.preventDefault();
+      hashtags.push(value.trim());
+      setValue("");
+    }
+  };
 
-const inputChangedHandler = (event) => {
-  setValue(event.target.value);
-};
+  const inputChangedHandler = (event) => {
+    setValue(event.target.value);
+  };
     
   let allusers = [];
 
@@ -67,86 +67,84 @@ const inputChangedHandler = (event) => {
   }
 
   return (
-    <div>
+  <div>
     <div style={{display: mode === "login" ? "block" : "none"}}>
       <div className={classes.LoginScreen}>
         <h2> <PersonIcon/> כניסה לאתר</h2>
         <hr color="#e6e6e6"/>
-      <TextField label="שם משתמש או אימייל"
-                 className={classes.Input}
-                 type="email"
-                 fullWidth={true}
-      /><br/><br/>
-      <TextField label="סיסמא"
-                 className={classes.Input}
-                 fullWidth={true}
-                 type="password"
-      />
-      <p>
-      <Button className={classes.LoginButton} 
-              variant="contained" 
-              onClick={props.close}
-              fullWidth={true}>
-        התחברות
-      </Button>
-      </p>
-      <br/><hr color="#e6e6e6"/>
-      <div className={classes.Center}>
-        <label>או כניסה דרך הרשתות החברתיות</label>
+        <TextField label="שם משתמש או אימייל"
+                  className={classes.Input}
+                  type="email"
+                  fullWidth={true}/><br/><br/>
+        <TextField label="סיסמא"
+                  className={classes.Input}
+                  fullWidth={true}
+                  type="password"/>
         <p>
-            <img className={classes.LogoIcon} src={FacebookIcon} alt="FaceBook"/>
-            <span> </span>
-            <img className={classes.LogoIcon} src={GoogleIcon} alt="Google"/>
+          <Button className={classes.LoginButton} 
+                  variant="contained" 
+                  onClick={props.close}
+                  fullWidth={true}>
+            התחברות
+          </Button>
         </p>
-        <Link onClick={changeToRegister}>אין לך חשבון?</Link>
-      </div>
+        <br/><hr color="#e6e6e6"/>
+        <div className={classes.Center}>
+          <label>או כניסה דרך הרשתות החברתיות</label>
+          <p>
+              <img className={classes.LogoIcon} src={FacebookIcon} alt="FaceBook"/>
+              <span> </span>
+              <img className={classes.LogoIcon} src={GoogleIcon} alt="Google"/>
+          </p>
+          <Link onClick={changeToRegister}>אין לך חשבון?</Link>
+        </div>
       </div>
     </div>
     <div style={{display: mode === "register" ? "block" : "none"}}>
-    <div className={classes.Center}>
-        <h3>אנחנו שמחים שבחרת להירשם לתביצוגית!</h3>
-        <h4>איזה סוג חשבון מתאים לך?</h4>
-      <div className={classes.Users}>{allusers}</div>
-      <p>
-      <Button variant="contained" onClick={props.close}>
-        ביטול
-      </Button>
-      <span> </span>
-      <Button className={classes.LoginButton}>המשך</Button>
-      </p>
-      <Link onClick={changeToLogin}>כבר יש לך חשבון?</Link>
+      <div className={classes.Center}>
+          <h3>אנחנו שמחים שבחרת להירשם לתביצוגית!</h3>
+          <h4>איזה סוג חשבון מתאים לך?</h4>
+        <div className={classes.Users}>{allusers}</div>
+        <p>
+          <Button variant="contained" onClick={props.close}>
+            ביטול
+          </Button>
+          <span> </span>
+          <Button className={classes.LoginButton}>המשך</Button>
+        </p>
+        <Link onClick={changeToLogin}>כבר יש לך חשבון?</Link>
       </div>
     </div>
     <div style={{display: mode === "user" ? "block" : "none"}}>
-      <div className={classes.Center}>
-        <h2> <PersonIcon/> הרשמה בתור משתמש לתביצוגית</h2>
-        <hr color="#e6e6e6"/>
-      </div>
-      <div className={classes.UserRegister}>
-        <TextField label="שם מלא"
-                   className={classes.Input}
-                   fullWidth={true}/><br/><br/>
-        <TextField label="שם משתמש"
-                   className={classes.Input}
-                   fullWidth={true}/><br/><br/>
-        <TextField label="אימייל"
-                   className={classes.Input}
-                   type="email"
-                   fullWidth={true}/><br/><br/>
-        <TextField label="סיסמא"
-                   className={classes.Input}
-                   fullWidth={true}
-                   type="password"/><br/><br/>
-        <TextField label="אימות סיסמא"
-                   className={classes.Input}
-                   fullWidth={true}
-                   type="password"/><br/><br/>
-        <Button className={classes.ProfileButton}>סיום הרשמה</Button>
-        <Button className={classes.BackButton} 
-                variant="contained" 
-                onClick={changeToRegister}>
-        חזור
-      </Button>
+        <div className={classes.Center}>
+          <h2> <PersonIcon/> הרשמה בתור משתמש לתביצוגית</h2>
+          <hr color="#e6e6e6"/>
+        </div>
+        <div className={classes.UserRegister}>
+          <TextField label="שם מלא"
+                    className={classes.Input}
+                    fullWidth={true}/><br/><br/>
+          <TextField label="שם משתמש"
+                    className={classes.Input}
+                    fullWidth={true}/><br/><br/>
+          <TextField label="אימייל"
+                    className={classes.Input}
+                    type="email"
+                    fullWidth={true}/><br/><br/>
+          <TextField label="סיסמא"
+                    className={classes.Input}
+                    fullWidth={true}
+                    type="password"/><br/><br/>
+          <TextField label="אימות סיסמא"
+                    className={classes.Input}
+                    fullWidth={true}
+                    type="password"/><br/><br/>
+          <Button className={classes.ProfileButton}>סיום הרשמה</Button>
+          <Button className={classes.BackButton} 
+                  variant="contained" 
+                  onClick={changeToRegister}>
+            חזור
+          </Button>
         </div>
       </div>
     <div style={{display: mode === "lawyer1" ? "block" : "none"}}>
@@ -178,16 +176,16 @@ const inputChangedHandler = (event) => {
         <Button className={classes.BackButton} 
                 variant="contained" 
                 onClick={changeToRegister}>
-        חזור
-      </Button>
-        </div>
+          חזור
+        </Button>
       </div>
-      <div style={{display: mode === "lawyer2" ? "block" : "none"}} 
-           className={classes.LawyerRegister}>
-        <div className={classes.Center}>
-          <h2> <GavelIcon/> יצירת פרופיל עו"ד בתביצוגית</h2>
-          <hr color="#e6e6e6"/>
-        </div>
+    </div>
+    <div style={{display: mode === "lawyer2" ? "block" : "none"}} 
+         className={classes.LawyerRegister}>
+      <div className={classes.Center}>
+        <h2> <GavelIcon/> יצירת פרופיל עו"ד בתביצוגית</h2>
+        <hr color="#e6e6e6"/>
+      </div>
       <TextField label="שם המשרד"
                  className={classes.Input}
                  fullWidth={true}/><br/><br/>
@@ -214,19 +212,19 @@ const inputChangedHandler = (event) => {
                  type="number"
                  className={classes.Input}/><br/><br/>
       <p>
-      <Button className={classes.ProfileButton} 
-              variant="contained" 
-              onClick={props.close}>
-        יצירת פרופיל
-      </Button>
-      <Button className={classes.BackButton} 
-              variant="contained" 
-              onClick={regiesterAsLawyer}>
-        חזור
-      </Button>
+        <Button className={classes.ProfileButton} 
+                variant="contained" 
+                onClick={props.close}>
+          יצירת פרופיל
+        </Button>
+        <Button className={classes.BackButton} 
+                variant="contained" 
+                onClick={regiesterAsLawyer}>
+          חזור
+        </Button>
       </p>
-      </div>
     </div>
+  </div>
   );
 };
 
