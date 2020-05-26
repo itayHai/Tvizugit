18a247824744f7ce37a9ce0d86e0c35d0af6a17b
tali cohen <talicohen1995@gmail.com>
2020-05-27 00:04:04 +0300

    remove dummyData

--- client/src/components/LawyersStock/Lawyers/Lawyers.js
+++ client/src/components/LawyersStock/Lawyers/Lawyers.js
@@ -8,33 +8,6 @@ import { dummyData } from '../../../utils/globalConsts';
 import { lawyersRequests } from "../../../utils/requests";
 import Lawyer from "./Lawyer/Lawyer";
 
-const lawyersData = [{
-        id : "5ec1109c32cbb037fad83823",
-        name: "המשרד של טלי",
-        description: "תיאור של המשרד",
-        expertise:["נזיקין", "חוזים", "חברות"],
-        email:"t@gmail.com",
-        address:"רחוב שקר כלשהו",
-        phone:"0546875959",
-        seniority:50,
-        img:{"$binary":{"base64":"","subType":"00"}},
-        classactions: dummyData,
-    },
-    {
-        id : "5ec1109c32cbb037fad83824",
-        name: "2 המשרד של טלי",
-        description: "תיאור של המשרד",
-        expertise:["נזיקין"],
-        email:"t@gmail.com",
-        address:"רחוב שקר כלשהו",
-        phone:"0546875959",
-        seniority:50,
-        img:{"$binary":{"base64":"","subType":"00"}},
-        classactions: dummyData,
-        },
-
-];
-
 const Lawyers = (props) => {
     const { loading, error, data } = useQuery(
         lawyersRequests.getAllLawyers
@@ -50,12 +23,11 @@ const Lawyers = (props) => {
 
     return (
         <div>
-            {lawyersData.length === 0 ? "לא קיימים משרדי עורכי דין במאגר" : ""}
+            {data.LawyerQueries.lawyers.length === 0 ? "לא קיימים משרדי עורכי דין במאגר" : ""}
             {lawyersElements}
         </div>
     );
 };
-
 
 
 export default Lawyers;
