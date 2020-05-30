1032f0f3beabd5db5ded592e149feb4567ebc97c
ellaportnov <ellaportnov@gmail.com>
2020-05-30 11:15:09 +0300

    now this works

--- client/src/components/classActionsByFilter/classActionCard/classActionCard.js
+++ client/src/components/classActionsByFilter/classActionCard/classActionCard.js
@@ -1,38 +1,48 @@
-import React from 'react'
-import Card from '@material-ui/core/Card'
-import CardActions from '@material-ui/core/CardActions'
-import CardContent from '@material-ui/core/CardContent'
-import Button from '@material-ui/core/Button'
-import people_icon from '../../../images/icons/people_icon.png'
-import trophy_icon from '../../../images/icons/trophy_icon.png'
-import classes from './classActionCard.module.css'
+import React from "react";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import people_icon from "../../../images/icons/people_icon.png";
+import trophy_icon from "../../../images/icons/trophy_icon.png";
+import classes from "./classActionCard.module.css";
+import { useHistory } from "react-router-dom";
+import { changeFilter } from "../../../store/classAction";
+import { useDispatch } from "react-redux";
 
-const classActionCard = ({ classAction }) => {
-    const { name, users, successChances } = classAction
+const ClassActionCard = ({ classAction }, props) => {
+  const { name, users, successChances } = classAction;
+  const history = useHistory();
+  const dispatch = useDispatch();
 
-    return (
-        <Card className={classes.root}>
-            <CardContent>
-                <label className={classes.title}>
-                    {name}
-                </label>
-                <hr className={classes.underlineTitle} />
-                <br />
-                <img className={classes.icon} src={people_icon} alt="members" />
-                <label>
-                    <b>{users.length}</b> כבר הצטרפו למאבק
-                </label>
-                <br />
-                <img className={classes.icon} src={trophy_icon} alt="trophy" />
-                <label>
-                    <b>{successChances}</b> סיכויי זכייה
-                </label>
-            </CardContent>
-            <CardActions>
-                <Button size="small">קרא עוד</Button>
-            </CardActions>
-        </Card>
-    )
-}
+  const readMoreClicked = () => {
+    dispatch(changeFilter({ name: name, categories: [], hashtags: [] }));
+    history.push("/classActionsStock/");
+  };
 
-export default classActionCard
+  return (
+    <Card className={classes.root}>
+      <CardContent>
+        <label className={classes.title}>{name}</label>
+        <hr className={classes.underlineTitle} />
+        <br />
+        <img className={classes.icon} src={people_icon} alt="members" />
+        <label>
+          <b>{users.length}</b> כבר הצטרפו למאבק
+        </label>
+        <br />
+        <img className={classes.icon} src={trophy_icon} alt="trophy" />
+        <label>
+          <b>{successChances}</b> סיכויי זכייה
+        </label>
+      </CardContent>
+      <CardActions>
+        <Button size="small" onClick={readMoreClicked}>
+          קרא עוד
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default ClassActionCard;
