b16800655ecd29fb1ab354cac3667f83468c2529
ellaportnov <ellaportnov@gmail.com>
2020-06-04 11:07:35 +0300

    added error catching in homepage

--- client/src/components/homePage/homePage.js
+++ client/src/components/homePage/homePage.js
@@ -6,11 +6,16 @@ import ViewerHomePage from "./viewerHomePage/viewerHomePage"
 import { useSelector } from "react-redux";
 import PlaintiffHomePage from "./plaintiffHomePage/plaintiffHomePage";
 import { classActionsFilters } from "../../utils/globalConsts"
+import {ErrorBoundary} from 'react-error-boundary'
+import ErrorPage from '../errorPage/errorPage';
+
 
 export default function HomePage() {
   const loggedInUser = useSelector((state) => state.user.loggedInUser);
   return (
     <div>
+      <ErrorBoundary FallbackComponent={ErrorPage}>
+
       {(Object.keys(loggedInUser).length === 0) &&
         <ViewerHomePage />}
       {
@@ -23,6 +28,7 @@ export default function HomePage() {
         <p className={classes.classActionTitle}>קטגוריות התובענות</p>
         <CategoriesCards homePage={true}/>
       </article>
+      </ErrorBoundary>
     </div>
   );
 }
