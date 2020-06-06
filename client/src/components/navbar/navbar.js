3b5a6418c9892f6e5ec60641b74bdd97b212cf13
rotemhugi <rotemhugi@gmail.com>
2020-06-06 10:19:44 +0300

    add select user by email

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -7,11 +7,13 @@ import { withRouter } from "react-router";
 import { Link } from "react-router-dom";
 import ManageLogin from "../login/manageLogin";
 import Modal from "../modal/modal";
-import { setMode } from '../../store/user';
+import { setMode, changeLoggedInUser } from '../../store/user';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import CustomerIcon from '../../images/icons/customer_icon.png';
+import LogoutIcon from '../../images/icons/logout.png';
 import './navbar.css';
+import { changeFilter } from "../../store/classAction";
 
 const Navbar = (props) => {
   const loggedInUser = useSelector(state => state.user.loggedInUser)
@@ -27,39 +29,50 @@ const Navbar = (props) => {
     setOpen(false);
   };
 
-  const show = Object.keys(loggedInUser).length !== 0 ? 
-  <div>
-    <img className="Icon" src={CustomerIcon} alt="Customer"/>
-    <label> { loggedInUser.displayName } </label>
-  </div> :
-  <Button onClick={handleOpen} 
-          className="login"
-          variant="contained"> <PersonIcon/> כניסה  </Button> ;
+  const logout = () => {
+    dispatch(changeLoggedInUser({}));
+  };
+
+  const show = Object.keys(loggedInUser).length !== 0 ?
+    <div>
+      <img className="Icon" src={CustomerIcon} alt="Customer" />
+      <label> {loggedInUser.displayName} <img onClick={logout} className="Icon" src={LogoutIcon} alt="Logout" /></label>
+    </div> :
+    <Button onClick={handleOpen}
+      className="login"
+      variant="contained"> <PersonIcon /> כניסה  </Button>;
+
 
   return (
-      <AppBar position="static">
-        <Toolbar className="navbar">
-          <Link to="/" className="link">
-            <GavelIcon className="icon" fontSize="large"/>
-            <div className="titleContainer">
-              <label className="mainTitle">תביצוגית</label>
-              <label className="slogan">לתבוע בידיים טובות</label>
-            </div>
-          </Link>
-          <Link to="/classActionsStock/all" className="link" >
-            <h3>מאגר התביעות</h3>
-          </Link>
-          <Link to="/lawyers" className="link" >
-            <h3>מאגר עורכי הדין</h3>
-          </Link>
-          <div className="login">
-            {show}
+    <AppBar position="static">
+      <Toolbar className="navbar">
+        <Link to="/" className="link">
+          <GavelIcon className="icon" fontSize="large" />
+          <div className="titleContainer">
+            <label className="mainTitle">תביצוגית</label>
+            <label className="slogan">לתבוע בידיים טובות</label>
           </div>
-          <Modal show={open} onClose={handleClose}>
-              <ManageLogin close={handleClose} />
-            </Modal>
+        </Link>
+        <Link to="/classActionsStock/" className="link" onClick={() => dispatch(changeFilter())}>
+          <h3>מאגר התביעות</h3>
+        </Link>
+        <Link to="/lawyers" className="link" >
+          <h3>מאגר עורכי הדין</h3>
+        </Link>
+        {
+          (Object.keys(loggedInUser).length !== 0 && loggedInUser.role.engName === "admin") &&
+          < Link to="/reportedClassActions" className="link" >
+            <h3>ניהול תובענות מדווחות</h3>
+          </Link>
+        }
+      <div className="login">
+        {show}
+      </div>
+      <Modal show={open} onClose={handleClose}>
+        <ManageLogin close={handleClose} />
+      </Modal>
         </Toolbar>
-      </AppBar>
+      </AppBar >
   );
 };
 
